c4e7191411daffa6fec1b7086c24d903b4952698
PineconeChamp <syd@hanby.org.uk>
2026-05-17 19:13:07 +0100

    Added acceleration and basic controls to the car

--- raycast_car.gd
+++ raycast_car.gd
@@ -1,41 +1,70 @@
 extends RigidBody3D
 
-@export var wheels: Array[RayCast3D]
+@export var wheels: Array[RaycastWheel]
+@export var acceleration := 600.0
 
-# Values for a car mass of 900KG
-@export var spring_strength := 20000.0
-@export var spring_damping := 1500.0
-@export var rest_dist := 0.425
-@export var wheel_radius := 0.4
-@export var stick_factor := 0.15
+var motor_input := 0.0
+
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if event.is_action("accelerate"):
+		motor_input = event.get_action_strength("accelerate")
+	elif event.is_action_released("accelerate"):
+		motor_input = 0
+	
+	if event.is_action("brake"):
+		var brake_force = event.get_action_strength("brake")
+		brake_force = -brake_force
+		motor_input = brake_force
+	elif event.is_action_released("brake"):
+		motor_input = 0
+		
 
 func _physics_process(delta: float) -> void:
-	for wheel in wheels: _do_single_wheel_suspension(wheel)
+	for wheel in wheels: 
+		_do_single_wheel_suspension(wheel)
+		_do_single_wheel_acceleration(wheel)
+
 
 func _get_point_velocity(point: Vector3) -> Vector3:
 	return linear_velocity + angular_velocity.cross(point - global_position)
-
-func _do_single_wheel_suspension(suspension_ray: RayCast3D) -> void:
-	if suspension_ray.is_colliding():
+	
+	
+func _do_single_wheel_acceleration(ray: RaycastWheel) -> void:
+	if ray.is_colliding() and ray.is_motor:
+		var forward_dir := -ray.global_basis.z
+		var contact := ray.wheel.global_position
+		var force_vector := forward_dir * acceleration * motor_input
+		print(motor_input)
+		print(forward_dir*acceleration)
+		var force_pos := contact - global_position
 		
-		suspension_ray.target_position.y = -(rest_dist + wheel_radius + stick_factor)
-		var contact := suspension_ray.get_collision_point()
-		var spring_up_dir := suspension_ray.global_transform.basis.y
-		var spring_len := suspension_ray.global_position.distance_to(contact) - wheel_radius
-		var offset := rest_dist - spring_len
+		apply_force(force_vector, force_pos)
+		DebugDraw3D.draw_arrow_ray(contact, force_vector/mass, 2.5, Color.RED, 0.1)
 		
-		suspension_ray.get_node("Wheel").position.y = -spring_len
 		
-		var spring_force := spring_strength * offset
+func _do_single_wheel_suspension(ray: RaycastWheel) -> void:
+	if ray.is_colliding():
+		
+		ray.target_position.y = -(ray.rest_distance + ray.wheel_radius + ray.stick_factor)
+		var contact := ray.get_collision_point()
+		var spring_up_dir := ray.global_transform.basis.y
+		var spring_len := ray.global_position.distance_to(contact) - ray.wheel_radius
+		var offset := ray.rest_distance - spring_len
+		
+		ray.wheel.position.y = -spring_len
+		
+		var spring_force := ray.spring_strength * offset
 		
 		#Damping force = damping * velative velocity
 		var world_vel := _get_point_velocity(contact)
 		var relative_vel := spring_up_dir.dot(world_vel)
-		var spring_damp_force := spring_damping * relative_vel
+		var spring_damp_force := ray.spring_damping * relative_vel
 		
 		var force_vector := (spring_force - spring_damp_force) * spring_up_dir
 		
+		contact = ray.wheel.global_position
 		var force_pos_offset := contact - global_position
 		apply_force(force_vector, force_pos_offset)
 		
-		#DebugDraw.draw_arrow_ray(contact, force_vector, 2.5)
+		DebugDraw3D.draw_arrow_ray(contact, force_vector/mass, 2, Color.LIME_GREEN, 0.1)
---
+++ raycast_wheel.gd
@@ -0,0 +1,11 @@
+extends RayCast3D
+class_name RaycastWheel
+
+@export var spring_strength := 100
+@export var spring_damping := 2.0
+@export var rest_distance := 0.5
+@export var stick_factor := 0.0
+@export var wheel_radius := 0.4
+@export var is_motor := false
+
+@onready var wheel: Node3D = get_child(0)
